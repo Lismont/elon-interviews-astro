cb5ba8584be796712a6f19e9824e8bbe2ebc6992
Jonas Lismont <jonas@lismont.net>
2024-03-28 12:40:57 +0100

    l

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+    prefetch: true,
+    image: {
+      remotePatterns: [{
+          protocol: 'https',
+          hostname: '**.amazonaws.com',
+      }],
+  },
 });
